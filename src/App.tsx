09d0165497a05c2226cc08b369f8b41534de6d17
alexandre-jr-94 <alexandre.andriani.jr@gmail.com>
2022-10-09 22:25:14 -0300

    Jogo da cor

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [color, setColor] = useState<string>();
+  const [corBotao, setCorBotao] = useState<string[]>([]);
+  const [errado, setErrado] = useState(false);
+
+  /* Gerar nº Hex */
+  /* 16777215 == ffffff */
+  const corAleatoria = () => {
+    return "#" + Math.floor(Math.random() * 16777215).toString(16);
+  };
+
+  const corDoBloco = () => {
+    const cor = corAleatoria();
+    setColor(cor);
+    setCorBotao(
+      [
+        cor,
+        corAleatoria(),
+        corAleatoria(),
+        corAleatoria(),
+        corAleatoria(),
+      ].sort(function () {
+        return 0.5 - Math.random();
+      })
+    );
+  };
+
+  useEffect(() => {
+    corDoBloco();
+  }, []);
+
+  /* Informa se está errado ou não e gera nova cor caso esteja correto */
+  function click(resposta: string) {
+    if (resposta === color) {
+      setErrado(false);
+      corDoBloco();
+    } else {
+      setErrado(true);
+    }
+  }
+  /* Página  */
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="adivinha">
+        <div className="cor" style={{ background: color }}></div>
+        <div className="botaoarea">
+          {corBotao.map((resposta) => (
+            <button
+              onClick={() => click(resposta)}
+              key={resposta}
+              className="botao"
+            >
+              {resposta}
+            </button>
+          ))}
+          {errado && <div className="errou">Tente Novamente!</div>}
+        </div>
+      </div>
     </div>
   );
 }
